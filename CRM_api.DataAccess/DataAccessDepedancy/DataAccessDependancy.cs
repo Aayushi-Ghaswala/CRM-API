4079beca270d74e61b2c0b59055945b9d8f096c4
Aayushi Ghaswala <aayushi@fuzionagency.com>
2023-07-10 15:47:20 +0530

    Needed push.

--- CRM_api.DataAccess/DataAccessDepedancy/DataAccessDependancy.cs
+++ CRM_api.DataAccess/DataAccessDepedancy/DataAccessDependancy.cs
@@ -70,7 +70,7 @@ namespace CRM_api.DataAccess.DataAccessDepedancy
             //services.AddScoped<IStatusRepository, StatusRepository>();
             services.AddScoped<ISourceTypeRepository, SourceTypeRepository>();
             services.AddScoped<ISourceRepository, SourceRepository>();
-            //services.AddScoped<ICampaignRepository, CampaignRepository>();
+            services.AddScoped<ICampaignRepository, CampaignRepository>();
             //services.AddScoped<IMeetingRepository, MeetingRepository>();
             //services.AddScoped<IMeetingAttachmentRepository, MeetingAttachmentRepository>();
             //services.AddScoped<IMeetingParticipantRepository, MeetingParticipantRepository>();
---
+++ CRM_api.Services/Dtos/ResponseDto/Sales Module/MeetingDto.cs
@@ -0,0 +1,24 @@
+﻿using CRM_api.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_api.Services.Dtos.ResponseDto.Sales_Module
+{
+    public class MeetingDto
+    {
+        public int Id { get; set; }
+        public TblUserMaster MeetingBy { get; set; }
+        public string Purpose { get; set; }
+        public DateTime DateOfMeeting { get; set; }
+        public Double Duration { get; set; }
+        public string Mode { get; set; }
+        public string Location { get; set; }
+        public string Remarks { get; set; }
+        public string Link { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
