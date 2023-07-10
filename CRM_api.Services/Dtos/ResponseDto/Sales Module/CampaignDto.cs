7d2dd2fc862367fe5d0437f403916d7c2c994ed2
Aayushi Ghaswala <aayushi@fuzionagency.com>
2023-07-10 15:45:43 +0530

    Implemented APIs through which admin can manage Campaign entries.

---
+++ CRM_api.Services/Dtos/ResponseDto/Sales Module/CampaignDto.cs
@@ -0,0 +1,22 @@
+﻿using CRM_api.DataAccess.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM_api.Services.Dtos.ResponseDto.Sales_Module
+{
+    public class CampaignDto
+    {
+        public int Id { get; set; }
+        public TblUserMaster TblUserMaster { get; set; }
+        public TblSourceTypeMaster TblSourceTypeMaster { get; set; }
+        public TblSourceMaster TblSourceMaster { get; set; }
+        public TblStatusMaster TblStatusMaster { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public decimal Budget { get; set; }
+        public decimal RevenueExpected { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ CRM_api.Services/Services/Sales Module/CampaignService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using CRM_api.DataAccess.Helper;
+using CRM_api.DataAccess.IRepositories.Sales_Module;
+using CRM_api.DataAccess.Models;
+using CRM_api.Services.Dtos.AddDataDto.Sales_Module;
+using CRM_api.Services.Dtos.ResponseDto.Generic_Response;
+using CRM_api.Services.Dtos.ResponseDto.Sales_Module;
+using CRM_api.Services.IServices.Sales_Module;
+
+namespace CRM_api.Services.Services.Sales_Module
+{
+    public class CampaignService : ICampaignService
+    {
+        private readonly ICampaignRepository _campaignRepository;
+        private readonly IMapper _mapper;
+
+        public CampaignService(ICampaignRepository campaignRepository, IMapper mapper)
+        {
+            _campaignRepository = campaignRepository;
+            _mapper = mapper;
+        }
+
+        #region Get Campaigns
+        public async Task<ResponseDto<CampaignDto>> GetCampaignsAsync(string search, SortingParams sortingParams)
+        {
+            var campaigns = await _campaignRepository.GetCampaigns(search,sortingParams);
+            var mapCampaign = _mapper.Map<ResponseDto<CampaignDto>>(campaigns);
+            return mapCampaign;
+        }
+        #endregion
+
+        #region Get Campaign By Id
+        public async Task<CampaignDto> GetCampaignByIdAsync(int id)
+        {
+            var campaign = await _campaignRepository.GetCampaignById(id);
+            var mappedCampaign = _mapper.Map<CampaignDto>(campaign);
+            return mappedCampaign;
+        }
+        #endregion
+
+        #region Get Campaign By Name
+        public async Task<CampaignDto> GetCampaignByNameAsync(string Name)
+        {
+            var campaign = await _campaignRepository.GetCampaignByName(Name);
+            var mappedCampaign = _mapper.Map<CampaignDto>(campaign);
+            return mappedCampaign;
+        }
+        #endregion
+
+        #region Add Campaign
+        public async Task<int> AddCampaignAsync(AddCampaignDto campaignDto)
+        {
+            var campaign = _mapper.Map<TblCampaignMaster>(campaignDto);
+            return await _campaignRepository.AddCampaign(campaign);
+        }
+        #endregion
+
+        #region Update Campaign
+        public async Task<int> UpdateCampaignAsync(UpdateCampaignDto campaignDto)
+        {
+            var campaign = _mapper.Map<TblCampaignMaster>(campaignDto);
+            return await _campaignRepository.UpdateCampaign(campaign);
+        }
+        #endregion
+
+        #region Deactivate Campaign
+        public async Task<int> DeactivateCampaignAsync(int id)
+        {
+            return await _campaignRepository.DeactivateCampaign(id);
+        }
+        #endregion
+    }
+}
