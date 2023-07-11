045601c157496f5dfdb9e7aaafdcb9e7779e3e81
Aayushi-Ghaswala <121868023+Aayushi-Ghaswala@users.noreply.github.com>
2023-07-11 10:28:46 +0530

    Update ILeadService.cs

--- CRM_api.Services/IServices/Sales Module/ILeadService.cs
+++ CRM_api.Services/IServices/Sales Module/ILeadService.cs
@@ -1,5 +1,5 @@
 ﻿using CRM_api.DataAccess.Helper;
-using CRM_api.DataAccess.RequestModel.Sales_Module;
+using CRM_api.Services.Dtos.ResponseDto.Business_Module.LI_GI_Module;
 using CRM_api.Services.Dtos.AddDataDto.Sales_Module;
 using CRM_api.Services.Dtos.ResponseDto.Generic_Response;
 using CRM_api.Services.Dtos.ResponseDto.Sales_Module;
@@ -10,7 +10,7 @@ namespace CRM_api.Services.IServices.Sales_Module
     public interface ILeadService
     {
         Task<ResponseDto<LeadDto>> GetLeadsAsync(string search, SortingParams sortingParams);
-        Task<ResponseDto<InvestmentTypeDto>> GetInvestmentTypesAsync(string search, SortingParams sortingParams);
+        Task<ResponseDto<InvesmentTypeDto>> GetInvestmentTypesAsync(string search, SortingParams sortingParams);
         Task<LeadDto> GetLeadByIdAsync(int id);
         Task<LeadDto> GetLeadByNameAsync(string Name);
         Task<int> AddLeadAsync(AddLeadDto leadDto);
