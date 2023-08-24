0c47fe4992c94bc8237517a27e2dfd3b22582ecf
DipKavaiya <dippanchal02@gmail.com>
2023-08-24 18:33:58 +0530

    Updated Variable names

--- CRM_api.Services/Dtos/ResponseDto/Business Module/MutualFunds Module/MFTransactionDto.cs
+++ CRM_api.Services/Dtos/ResponseDto/Business Module/MutualFunds Module/MFTransactionDto.cs
@@ -7,7 +7,7 @@ namespace CRM_api.Services.Dtos.ResponseDto.Business_Module.MutualFunds_Module
         public ResponseDto<T>? response { get; set; }
         public decimal? totalBalanceUnit { get; set; }
         public decimal? totalPurchaseAmount { get; set; }
-        public decimal? totalRedemAmount { get; set; }
+        public decimal? totalRedeemAmount { get; set; }
         public decimal? totalAmount { get; set; }
         public decimal? totalScheme { get; set;}
     }
