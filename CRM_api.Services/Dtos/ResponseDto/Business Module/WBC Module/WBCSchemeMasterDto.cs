1d3eb8147ab30592e845bd4a5053328b0b5aa8e1
iDhruvi <92042001+iDhruvi@users.noreply.github.com>
2023-10-10 11:04:52 +0530

    Update WBCSchemeMasterDto.cs

--- CRM_api.Services/Dtos/ResponseDto/Business Module/WBC Module/WBCSchemeMasterDto.cs
+++ CRM_api.Services/Dtos/ResponseDto/Business Module/WBC Module/WBCSchemeMasterDto.cs
@@ -13,6 +13,7 @@ namespace CRM_api.Services.Dtos.ResponseDto.Business_Module.WBC_Module
         public int? GoldPoint { get; set; }
         public DateTime? FromDate { get; set; }
         public DateTime? ToDate { get; set; }
+        public int? On_the_spot_GP { get; set; }
         public virtual WbcTypeDto? TblWbcTypeMaster { get; set; }
         public virtual SubInvestmentTypeDto? TblSubInvesmentType { get; set; }
         public virtual SubsubInvTypeDto? TblSubsubInvType { get; set; }
